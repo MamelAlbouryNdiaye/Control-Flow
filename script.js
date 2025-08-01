fc844c8c09e6b9f43868f3540ba4709bb5140ee4
Mamel Alboury NDIAYE <Mamel Aly>
2025-08-01 05:04:32 +0200

    maximum plant capacity

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const PI = 3.1415;
 const radius = 5;
-const area = PI * radius * radius; // Aire du jardin
-const plantSpace = 0.8; // espace requis par plante
+const area = PI * radius * radius; 
+const plantSpace = 0.8; 
 const initialPlants = 20;
+
+const maxCapacity = Math.floor(area / plantSpace);// maximun plants capacity
